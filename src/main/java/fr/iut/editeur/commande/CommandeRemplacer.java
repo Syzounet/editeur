ec9174972a978779a1365c286210f91c71592188
Gauthier souquet <gauthier.souquet@etu.umontpellier.Fr>
2024-09-03 14:36:37 +0200

    fix : correction bug paramètre 3

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -18,9 +18,14 @@ public class CommandeRemplacer extends CommandeDocument{
 
         int debut = Integer.parseInt(parameters[1]); // permet de récupérer le premier paramètre de la commande
         int fin = Integer.parseInt(parameters[2]);   // permet de récupérer le deuxième paramètre de la commande
-        String remplacement = parameters[3];
-        this.document.remplacer(debut, fin, remplacement); // permet de remplacer le texte
-        super.executer();
+        if (parameters[3].equals("")){
+            this.document.remplacer(debut, fin, "");
+            super.executer();
+        }else {
+            String remplacement = parameters[3]; // permet de récupérer le troisième paramètre de la commande
+            this.document.remplacer(debut, fin, remplacement); // permet de remplacer le texte
+            super.executer();
+        }
 
     }
 }
