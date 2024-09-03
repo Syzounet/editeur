9a198f4dd5540991537d461fc53d6d6447ff8bfa
Gauthier souquet <gauthier.souquet@etu.umontpellier.Fr>
2024-09-03 14:24:48 +0200

    fix :  bug des paramètres debut et fin

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -1,2 +1,26 @@
-package fr.iut.editeur.commande;public class CommandeRemplacer {
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument{
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer(){
+        if(parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;debut;fin;remplacement");
+            return;
+        }
+
+
+        int debut = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+        String remplacement = parameters[3];
+        this.document.remplacer(debut, fin, remplacement);
+        super.executer();
+
+    }
 }
